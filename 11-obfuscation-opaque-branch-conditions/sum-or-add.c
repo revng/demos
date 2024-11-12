63c72dd203aac80abcc9f030bba758f8fd9ddcfc
Pietro Fezzardi <pietro@rev.ng>
2024-11-12 09:33:30 +0100

    Deobfuscation: handle opaque branch conditions

---
+++ 11-obfuscation-opaque-branch-conditions/sum-or-add.c
@@ -0,0 +1,15 @@
+long obfuscated_add_42(long value) {
+  return value + 42;
+}
+
+long add_or_sum(long arg1, long arg2) {
+  if (obfuscated_add_42(33) > 50) {
+    return arg1 + arg2;
+  } else {
+    return arg1 << arg2;
+  }
+}
+
+long _start(long a, long b) {
+  return add_or_sum(a, b);
+}
